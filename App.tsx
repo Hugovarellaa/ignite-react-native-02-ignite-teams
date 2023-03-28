fb3eaf43bb6349ac5cca0633214ac9970c2af685
hugo <hugovarellaa@gmail.com>
2023-03-28 15:28:19 -0300

    feat: create component loading

--- App.tsx
+++ App.tsx
@@ -1,11 +1,22 @@
+import { Loading } from "@components/Loading";
+import {
+  Roboto_400Regular,
+  Roboto_700Bold,
+  useFonts,
+} from "@expo-google-fonts/roboto";
 import { Group } from "@screens/Group";
 import theme from "@theme/index";
 import { ThemeProvider } from "styled-components";
 
 export default function App() {
+  const [fontsLoading] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+  });
+
   return (
     <ThemeProvider theme={theme}>
-      <Group />
+      {fontsLoading ? <Group /> : <Loading />}
     </ThemeProvider>
   );
 }
