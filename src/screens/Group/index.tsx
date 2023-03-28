f862ae5a28fa955a66b27b213030e4d3a927a57a
hugo <hugovarellaa@gmail.com>
2023-03-28 16:12:19 -0300

    feat: using header and highlight components in Group

--- src/screens/Group/index.tsx
+++ src/screens/Group/index.tsx
@@ -1,10 +1,12 @@
 import { Header } from "@components/Header";
+import { HighLight } from "@components/HighLight";
 import { Container } from "./styles";
 
 export function Group() {
   return (
     <Container>
       <Header />
+      <HighLight title="Turmas" subTitle="jogue com a sua turma" />
     </Container>
   );
 }
