12750ded1336311faa5646284e37dbd47855de25
Hugo Alves Varella <hugovarellaa@gmail.com>
2023-03-31 12:13:17 -0300

    Delete src directory

--- src/screens/Group/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Text, View } from 'react-native'
-
-export default function Group() {
-  return (
-    <View>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
-  )
-}
