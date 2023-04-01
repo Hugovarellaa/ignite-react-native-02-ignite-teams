3ea0aac7487bf1572576bf8ac929c31554e74960
hugovarellaa <hugovarellaa@gmail.com>
2023-04-01 15:27:11 -0300

    First commit project

---
+++ ignite-teams/App.tsx
@@ -0,0 +1,11 @@
+import { Group } from "@screens/Group";
+import { StatusBar } from "expo-status-bar";
+
+export default function App() {
+  return (
+    <>
+      <Group />
+      <StatusBar style="auto" />
+    </>
+  );
+}
---
+++ ignite-teams/src/screens/Group/index.tsx
@@ -0,0 +1,9 @@
+import { Container, Title } from "./styles";
+
+export function Group() {
+  return (
+    <Container>
+      <Title>Open up App.tsx to start working on your app!</Title>
+    </Container>
+  );
+}
