2038dc8b0ec35e43bc77b7bf84874b8a0d5cfb1a
hugovarellaa <hugovarellaa@gmail.com>
2023-04-01 15:30:48 -0300

    First commit project

--- src/screens/Group/index.tsx
+++ src/screens/Group/index.tsx
@@ -3,7 +3,7 @@ import { Container, Title } from "./styles";
 export function Group() {
   return (
     <Container>
-      <Title>Open up App.tsx to start working on your app!</Title>
+      <Title>Testando</Title>
     </Container>
   );
 }
